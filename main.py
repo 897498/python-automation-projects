69177812765d0a919db3ed891dbfaedf72c58216
abdelrahman <[200~abdelrhmanelseidy80@gmail.com ~>
2026-04-15 14:57:22 +0200

    news automation script

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, I am Abdelrahman - AI Builder from Egypt!")
+import requests
+
+url = "https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=dffedc695fe8466c8255da9c7b159a3b"
+
+response = requests.get(url)
+data = response.json()
+
+print("=== Tech News Today ===\n")
+
+for article in data["articles"][:5]:
+    print("Title:", article["title"])
+    print("Source:", article["source"]["name"])
+    print("Link:", article["url"])
+    print("-" * 50)
